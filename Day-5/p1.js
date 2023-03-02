a33c4499fad8798d1751b7c31102b318c3cb15dc
Aman Kozybayev <Enphas@gmail.com>
2023-03-02 23:09:36 +0500

    Practice lesson 1

--- Day-5/p1.js
+++ Day-5/p1.js
@@ -1,3 +1,17 @@
 const obj = {
-    type : "small", index: 1
-}
+    type: "small",
+    index: 1, 
+    size: 2, 
+    companies: ["Coca-cola","Google", "Ford"],
+};
+
+const companies = {
+    data: ["Coca-cola", "Google", "Ford"], //data and length are the keys of the object 
+    length: 3, // on the right side of the key are values of the object 
+};
+
+const ar = ["A", "B"];
+console.log(companies.data);
+console.log(ar.length);
+
+console.log(obj.index);
