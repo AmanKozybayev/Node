a18272089786ea0cf73c71a976719e9015fb7ba9
Aman Kozybayev <Enphas@gmail.com>
2023-03-07 01:39:13 +0500

    saved changes

--- Day-6/db.js
+++ Day-6/db.js
@@ -1,8 +1,8 @@
-const users = [
+const users = [// declare array with objects of users 
     {id: 1, name: "Ali"},
     {id:2, name: "Ayauly"}
 ];
 
 module.exports = {
     users, // esli moy key i value odinakovye ya mogu napisat' tol'ko odin raz 
-}
+}// ya prosto dayu ponyat' chto modul' hranit v sebe list userov 
