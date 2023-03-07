b39daf83465beb77e279d7eea7cec2d5de120a91
Aman Kozybayev <Enphas@gmail.com>
2023-03-07 18:44:34 +0500

    self study using docs

---
+++ ss-1/p1.js
@@ -0,0 +1,13 @@
+// in this sheet i will practice with objects from javascript documentation 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+
+let x;//declared variable 
+
+if (1 !== 1 ) {//1 not equal to 1 ? 
+    x = { greeting: "hi there!"}; //if true x = {greeting: "hi there"}
+}
+else if (2===2) {//else if code above didn't execute check 2 equal to 2 ?
+    x = { sayBye: "Bye Bye"} // if true say Bye Bye 
+}
+
+console.log(x);//display bye bye
