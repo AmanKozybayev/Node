34ee91339032ea57f9076b6f6bf383a903261580
Aman Kozybayev <Enphas@gmail.com>
2023-03-09 14:16:57 +0500

    Math module

---
+++ Day-7 module/p1.js
@@ -0,0 +1,12 @@
+console.log(Math.PI);// internal module Logs: 3.141592653589793
+console.log(Math.sqrt(16)); // Logs: 4
+console.log(Math.abs(-123)); // Logs: 123
+console.log(Math.abs(123)); // Logs: 123
+console.log(Math.ceil(1.5));//Logs: 2
+console.log(Math.ceil(1.00002));//Logs: 2
+console.log(Math.ceil(1));//Logs: 1
+console.log(Math.floor(1.99999)); //Logs: 1 
+console.log(Math.round(1.3));// Logs: 1
+console.log(Math.round(1.5)); // Logs: 2
+console.log(Math.random()); // Logs: any random number between 0 and 1; 
+
