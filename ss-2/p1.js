8d50601929cefeabbd818ac916f5f00c1af0e4aa
Aman Kozybayev <Enphas@gmail.com>
2023-03-09 20:14:28 +0500

    Defining methods of the obj

---
+++ ss-2/p1.js
@@ -0,0 +1,21 @@
+// Defining methods 
+
+const myObj = {
+    car:"good",
+    myMethod: function (param) {
+        console.log(`Everything is ${this[param]}`)//doe etogo tut stoyal myObj[param] v sledstvii s drugimi prototipami eto ne rabotalo, poetomu ya ego zamenil na this i zarabotalo 
+},};
+
+myObj.myMethod('car'); // logs: Everything is good
+
+const myCar = Object.create(myObj); // kogda ya sozdayu object cherez const mozhno tol'ko tak sozdavat' protorypes
+
+myCar.car = "bad";
+console.log(myCar);//logs: { car: 'bad' }
+
+myCar.myMethod('car');
+
+
+
+
+
